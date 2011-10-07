a07eaa5e719c2fdc92eb7982f32c197fc3dbab9a
Johan Sjölin <johan.sjolin@gmail.com>
2011-10-07 11:47:23 +0200

    Initial Commit

---
+++ MOSA/MOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MOSAppDelegate.h
+//  MOSA
+//
+//  Created by Johan Sjölin on 10/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MOSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MOSA/MOSAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  MOSAppDelegate.m
+//  MOSA
+//
+//  Created by Johan Sjölin on 10/7/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MOSAppDelegate.h"
+
+@implementation MOSAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
