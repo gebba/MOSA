326439076a16db326579aa547f6f9e34f9491fa8
Johan Sjölin <johan.sjolin@gmail.com>
2011-10-11 22:34:41 +0200

    Added the MOSDownloadOperation for downloading the mine craft server binary.

---
+++ MOSA/MOSDownloadOperation.h
@@ -0,0 +1,13 @@
+//
+//  MOSDownloadOperation.h
+//  MOSA
+//
+//  Created by Johan Sjölin on 10/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MOSDownloadOperation : NSOperation
+
+@end
---
+++ MOSA/MOSDownloadOperation.m
@@ -0,0 +1,13 @@
+//
+//  MOSDownloadOperation.m
+//  MOSA
+//
+//  Created by Johan Sjölin on 10/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MOSDownloadOperation.h"
+
+@implementation MOSDownloadOperation
+
+@end
